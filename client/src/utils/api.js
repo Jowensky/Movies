c5d37679ea7821899630cf00bc9000d9f04503cc
Jowensky Neard <jp.12@live.com>
2019-03-06 01:02:45 -0500

    relative api request

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default {
   background: function() {
-    return axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=d3bd842cd067b7bd659924a258f4ce8d");
+    return axios.get("//api.themoviedb.org/3/trending/movie/week?api_key=d3bd842cd067b7bd659924a258f4ce8d");
   },
   poster: function(find) {
-    return axios.get("https://api.themoviedb.org/3/search/movie?api_key=d3bd842cd067b7bd659924a258f4ce8d", {
+    return axios.get("//api.themoviedb.org/3/search/movie?api_key=d3bd842cd067b7bd659924a258f4ce8d", {
       params: {
         query: find.query,
         year: find.year
@@ -13,10 +13,10 @@ export default {
     });
   },
   movieTitle: function() {
-    return axios.get("https://api.themoviedb.org/3/search/movie?api_key=d3bd842cd067b7bd659924a258f4ce8d");
+    return axios.get("//api.themoviedb.org/3/search/movie?api_key=d3bd842cd067b7bd659924a258f4ce8d");
   },
   youtube: function(options) {
-    return axios.get("https://www.googleapis.com/youtube/v3/search", {
+    return axios.get("//googleapis.com/youtube/v3/search", {
       params: {
         part: options.part,
         key: options.key,
@@ -27,6 +27,6 @@ export default {
     });
   },
   omdb: function(data) {
-    return axios.get("https://www.omdbapi.com/?apikey=a9f6407&t=" + data);
+    return axios.get("//omdbapi.com/?apikey=a9f6407&t=" + data);
   }
 };
