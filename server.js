768c96821676a425d11d4b6fe1fdb120f4fc2c39
Jowensky Neard <jp.12@live.com>
2019-05-07 14:49:16 -0400

    Movie Trailer

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// app.get("*", function(req,res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function(req,res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
