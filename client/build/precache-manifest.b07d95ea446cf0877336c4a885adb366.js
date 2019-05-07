ec56a2037884febb4941505307cdd8c8a9dfa68a
Jowensky Neard <jp.12@live.com>
2019-05-07 14:48:53 -0400

    Movie Trailer

---
+++ client/build/precache-manifest.b07d95ea446cf0877336c4a885adb366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a53c6da0ea4fa2cd0d64a950c554795",
+    "url": "/static/media/noPoster.3a53c6da.jpg"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/static/media/blankhead.b6681619.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "d93d7420c2076c7a221b",
+    "url": "/static/js/main.d93d7420.chunk.js"
+  },
+  {
+    "revision": "3d23535e1c9c310862a7",
+    "url": "/static/js/2.3d23535e.chunk.js"
+  },
+  {
+    "revision": "d93d7420c2076c7a221b",
+    "url": "/static/css/main.8ebfded2.chunk.css"
+  },
+  {
+    "revision": "908a853180b3fb20f0494913fcfe0e24",
+    "url": "/index.html"
+  }
+];
